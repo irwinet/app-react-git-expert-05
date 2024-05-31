24d892e02c6a46de4e6a5ac799d90f3a40a6530c
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-31 16:05:57 -0500

    Add GifExpertApp and styles

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react"
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch', 'Gragon Ball']);
+
+console.log(categories);
+
   return (
     <>
         <h1>GifExpertApp</h1>
+
+        <ol>
+            { categories.map(category => {
+                return <li key={category}>{category}</li>
+            }) }            
+        </ol>
+
     </>
   )
 }
